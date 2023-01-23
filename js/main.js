e055d7c9f08390fb7b7d542f5baf9234292ea57e
HadolphMercogliano <giovanni.mercogliano@gmail.com>
2023-01-23 10:30:36 +0100

    correzione lezione

--- js/main.js
+++ js/main.js
@@ -7,10 +7,17 @@ let result_psw;
 // VAR VALUE 
 
 user_name = prompt("Come ti chiami?");
+// .console.log('come ti chiami?' + username);
+
 surname = prompt("Qual è il tuo cognome?");
 fav_color = prompt("Qual è il tuo colore preferito");
 result_psw = user_name + surname + fav_color + "23";
 
 
 // RESULT WRITING
-document.getElementById('my_password').innerHTML = "La tua password è " + result_psw;
+// document.getElementById('my_password').innerHTML = "La tua password è " + result_psw;
+
+// TEMPLATE LITERALS 
+// (backtick ALT96)
+document.getElementById('my_password').innerHTML = `Grazie,
+la tua password è : ${result_psw}`
